a76ab8c93eb8332368e44dd9b1c201be540af44c
Amir U <cod3assassin@gmail.com>
2014-11-08 13:29:48 -0800

    fixed player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,9 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-// contribution Author: Eric Haines (Eric5h5) on Unify Community
-class PlayerController: MonoBehaviour {
+class PlayerController : MonoBehaviour {
 	public float moveSpeed = 3f;
 	public float gridSize = 1f;
-	public Vector3 direction;
-
 	private enum Orientation {
 		Horizontal,
 		Vertical
@@ -14,57 +11,71 @@ class PlayerController: MonoBehaviour {
 	private Orientation gridOrientation = Orientation.Horizontal;
 	private bool allowDiagonals = false;
 	private bool correctDiagonalSpeed = true;
-	private Vector2 input;
-	private bool isMoving = true;
-	private Vector3 startPosition;
-	private Vector3 endPosition;
+	public Vector2 input;
+	public Vector2 dir;
+	private bool isMoving = false;
+	public Vector3 startPosition;
+	public Vector3 endPosition;
 	private float t;
 	private float factor;
-	
 	public void Update() {
-		transform.position += transform.forward * moveSpeed * Time.deltaTime;
+	
+		if (!isMoving) {
+			input = new Vector2(Mathf.CeilToInt(Input.GetAxis("Horizontal")), Mathf.CeilToInt(Input.GetAxis("Vertical")));
+		/*	if (!allowDiagonals) {
+				if (Mathf.Abs(input.x) > Mathf.Abs(input.y)) {
+					input.y = 0;
+				} else {
+					input.x = 0;
+				}
+			}
+*/	      if(input.sqrMagnitude > 0)
+			{
+				Vector2 abs = new Vector2(Mathf.Abs(input.x), Mathf.Abs((input.y)));
+				if(abs.x > abs.y)
+				{
+					input.y = 0;
+				}
+				else
+				{
+					input.x = 0;
+				}
+			}
+			
+				StartCoroutine(move(transform));
 
-		if(Input.GetKeyDown(KeyCode.RightArrow))
-		{
-			direction = new Vector3(0,90,0);
 		}
-		if(Input.GetKeyDown(KeyCode.LeftArrow))
-		{
-			direction = new Vector3(0,-90,0);
-		}
-		if(Input.GetKeyDown(KeyCode.UpArrow))
-		{
-			direction = new Vector3(0,0,0);
-		}
-		if(Input.GetKeyDown(KeyCode.DownArrow))
-		{
-			direction = new Vector3(0,180,0);
-		}
-			transform.eulerAngles = direction;
 	}
 	
 	public IEnumerator move(Transform transform) {
 		isMoving = true;
 		startPosition = transform.position;
 		t = 0;
-		
-		if(gridOrientation == Orientation.Horizontal) {
-			endPosition = new Vector3(startPosition.x + System.Math.Sign(input.x) * gridSize,
-			                          startPosition.y, startPosition.z + System.Math.Sign(input.y) * gridSize);
-		} else {
-			endPosition = new Vector3(startPosition.x + System.Math.Sign(input.x) * gridSize,
-			                          startPosition.y + System.Math.Sign(input.y) * gridSize, startPosition.z);
+
+		if(input != Vector2.zero)
+		{
+			if(input.sqrMagnitude > 0)
+			{
+				dir.x = Mathf.RoundToInt(input.x); 
+				dir.y = Mathf.RoundToInt(input.y);     
+				dir.Normalize();
+			}
 		}
+
+		if(gridOrientation == Orientation.Horizontal) {
+			endPosition = new Vector3(startPosition.x + Mathf.RoundToInt(dir.x) * gridSize,
+			                          startPosition.y, startPosition.z + Mathf.RoundToInt(dir.y) * gridSize);
+		} 
 		
 		if(allowDiagonals && correctDiagonalSpeed && input.x != 0 && input.y != 0) {
 			factor = 0.7071f;
 		} else {
-			factor = 1.5f;
+			factor = 3.4f;
 		}
 		
 		while (t < 1f) {
 			t += Time.deltaTime * (moveSpeed/gridSize) * factor;
-			transform.position = Vector3.Lerp(startPosition, endPosition, t);
+			transform.position = Vector3.Lerp(startPosition, endPosition + new Vector3(dir.x,0,dir.y),t);
 			yield return null;
 		}
 		
